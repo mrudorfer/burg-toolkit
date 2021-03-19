44f3eb658cf31320c24af4bce2ed6a0a4d04c82a
martin.rudorfer <martin.rudorfer@tu-berlin.de>
2021-03-19 17:32:40 +0000

    extended poisson disk sampling so that it just takes a number of points as well

--- burg_toolkit/mesh_processing.py
+++ burg_toolkit/mesh_processing.py
@@ -28,9 +28,10 @@ def check_properties(mesh):
     print(f"  orientable:             {orientable}")
 
 
-def poisson_disk_sampling(mesh, radius=0.003, with_normals=True, init_factor=5):
+def poisson_disk_sampling(mesh, radius=0.003, n_points=None, with_normals=True, init_factor=5):
     """
     Performs poisson disk sampling.
+    Per default it uses the radius, but if `n_points` is given it just samples as many points.
     Ideally, we fit circles with a certain radius on the surface area of the mesh. The center points
     will then form the point cloud. In practice, we just randomly sample a certain number of points (depending
     on the surface area of the mesh, the radius and init_factor), then we eliminate points which do not fit well.
@@ -40,15 +41,17 @@ def poisson_disk_sampling(mesh, radius=0.003, with_normals=True, init_factor=5):
     :param init_factor: we will initially sample `init_factor` times more points than will be needed, if better
                         accuracy is desired this can be increased, if performance is important this should be decreased
     :param with_normals: whether or not the points shall have normals (default is True)
+    :param n_points: int, if given, it will simply sample as many points (which are approx evenly distributed)
 
     :return: open3d.geometry.PointCloud object
     """
-    # we assume the surface area to be square and use a square packing of circles
-    # the number n_s of circles along the side-length s can then be estimated with
-    s = np.sqrt(mesh.get_surface_area())
-    n_s = (s + 2*radius) / (2*radius)
-    n_points = int(n_s**2)
-    print(f'going for {n_points} points')
+    if n_points is None:
+        # we assume the surface area to be square and use a square packing of circles
+        # the number n_s of circles along the side-length s can then be estimated with
+        s = np.sqrt(mesh.get_surface_area())
+        n_s = (s + 2*radius) / (2*radius)
+        n_points = int(n_s**2)
+        print(f'going for {n_points} points')
 
     pc = mesh.sample_points_poisson_disk(
         number_of_points=n_points,
