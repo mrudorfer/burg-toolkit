1a6129634a0a5f4c00b15cc2754ba0390a21b113
martin.rudorfer <martin.rudorfer@tu-berlin.de>
2021-06-23 17:17:59 +0100

    renamed module camera pose generators to render

--- burg_toolkit/__init__.py
+++ burg_toolkit/__init__.py
@@ -4,12 +4,12 @@ BURG toolkit
 Benchmarking and Understanding Robotic Grasping
 """
 
-from . import camera_pose_generators
 from . import grasp
 from . import gripper
 from . import io
 from . import mesh_processing
 from . import metrics
+from . import render
 from . import sampling
 from . import scene
 from . import sim
@@ -17,12 +17,12 @@ from . import util
 from . import visualization
 
 __all__ = [
-    'camera_pose_generators',
     'grasp',
     'gripper',
     'io',
     'mesh_processing',
     'metrics',
+    'render',
     'sampling',
     'scene',
     'sim',
