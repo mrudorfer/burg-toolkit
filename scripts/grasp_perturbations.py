7d46dbad59371ae5ecfc33d9b08a96d27c8f4076
Martin Rudorfer <m.rudorfer@bham.ac.uk>
2021-08-26 14:38:05 +0100

    more efficient code, starting the simulator only once to process all perturbations. might be memory intensive though, expecting up to 1-2 million grasps

--- scripts/grasp_perturbations.py
+++ scripts/grasp_perturbations.py
@@ -1,8 +1,9 @@
 import os
 import argparse
-from collections import Counter
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 import burg_toolkit as burg
 
 try:
@@ -25,6 +26,10 @@ def parse_args():
                         help='where to put generated dataset files')
     parser.add_argument('-n', '--num_grasps', type=int, default=None,
                         help='number of grasps to process. up to first n successful grasps used. None processes all.')
+    parser.add_argument('--simulate', action='store_true', default=False,
+                        help='activate to perturb grasps and simulate all of them (takes time)')
+    parser.add_argument('--show', action='store_true', default=False,
+                        help='use this option to visualise perturbed grasps (taken from output_dir)')
     return parser.parse_args()
 
 
@@ -61,46 +66,109 @@ def perturb_and_simulate(grasps, gpnet_object_id):
     n_perturbations = 37
 
     scores = np.zeros(len(grasps))
-    overall_outcomes = Counter()
     overall_success = np.zeros(n_perturbations)  # so we can reason about certain perturbations
 
-    for i, grasp in enumerate(grasps):
-        perturbations = burg.sampling.grasp_perturbations(grasp, radii=[5, 10, 15])
+    perturbations = burg.sampling.grasp_perturbations(grasps, radii=[5, 10, 15])
+    conf = gpnet_sim.default_conf()
+    conf.z_move = True
+    success, outcomes = gpnet_sim.simulate_direct(
+        conf, gpnet_object_id, perturbations.translations, perturbations.quaternions)
 
-        conf = gpnet_sim.default_conf()
-        conf.z_move = True
-        success, outcomes = gpnet_sim.simulate_direct(
-            conf, gpnet_object_id, perturbations.translations, perturbations.quaternions)
-        scores[i] = np.mean(success)
-        overall_success += success
-        overall_outcomes += Counter(outcomes)
+    success = success.reshape(len(grasps), n_perturbations)
+    scores = np.mean(success, axis=-1)
+    overall_success = np.mean(success, axis=0)
 
     grasps.scores = scores
     print('***** SUMMARY *****')
     print('average robustness score:', np.mean(scores))
     print(f'max/min robustness score: {np.max(scores)}/{np.min(scores)}')
     print('overall outcomes,\tavg per grasp,\tavg per perturbation:')
-    for outcome, count in overall_outcomes.items():
+    for outcome, count in outcomes.items():
         print(f'\t{outcome}\t{count}\t{count/len(grasps)}\t{count/len(grasps)/n_perturbations} ')
-    print('overall success per perturbation (abs vs rel):')
+    print('overall success per perturbation:')
     for i, success in enumerate(overall_success):
-        print(f'\t{i}\t{success}\t{success/len(grasps)}')
+        print(f'\t{i}\t{success}')
 
-    return scores, overall_success, overall_outcomes
+    return scores, overall_success, outcomes
+
+
+def analyse_perturbation_success(success):
+    # we've got 37 fields in the array
+    print('*** perturbation analysis ***')
+    print('original grasps:', success[0])
+
+    # the loop goes like this:
+    # - radius (5, 10, 15)
+    #   - axis (trans xyz then rot xyz)
+    #       - plus, then minus
+    for i, label in enumerate(['x_trans', 'y_trans', 'z_trans', 'x_rot', 'y_rot', 'z_rot']):
+        sum = 0
+        for r, rad in enumerate(['5', '10', '15']):
+            idx_pos = 1 + 2*i + 12*r
+            idx_neg = idx_pos + 1
+            sum += success[idx_pos] + success[idx_neg]
+            print(f'{label}_{rad}:\t{(success[idx_pos] + success[idx_neg])/2}')
+            if label == 'z_trans':
+                print(f'\tfarther: \t{success[idx_pos]}')
+                print(f'\tcloser: \t{success[idx_neg]}')
+        print(f'{label}_avg:\t{sum/6}')
+
+
+def visualise_perturbation_success(success):
+    gs = burg.grasp.GraspSet.from_translations(np.asarray([0, 0, 0]).reshape(-1, 3))
+    gs_perturbed = burg.sampling.grasp_perturbations(gs[0], radii=[5, 10, 15])
+    gs_perturbed.scores = success
+    gripper = burg.gripper.ParallelJawGripper()
+    burg.visualization.show_grasp_set([], gs_perturbed, gripper=gripper,
+                                      score_color_func=lambda s: [1-s, s, 0])
 
 
 if __name__ == "__main__":
     print('hi')
+    print('args:')
     args = parse_args()
+    for key, value in vars(args).items():
+        print(f'\t{key}:\t{value}')
+    print('****')
+
     grasps = load_successful_grasps(args.shape, args.gpnet_data, args.model_dir)
     if args.num_grasps is not None:
         grasps = grasps[:args.num_grasps]
-    robustness_scores, perturbation_success, outcomes = perturb_and_simulate(grasps, args.shape)
 
-    burg.io.make_sure_directory_exists(args.output_dir)
+    if args.simulate is True:
+        robustness_scores, perturbation_success, outcomes = perturb_and_simulate(grasps, args.shape)
 
-    np.save(os.path.join(args.output_dir, args.shape + '_scores.npy'), robustness_scores)
-    np.save(os.path.join(args.output_dir, args.shape + '_success_p.npy'), perturbation_success)
-    np.save(os.path.join(args.output_dir, args.shape + '_outcomes.npy'), outcomes)
+        burg.io.make_sure_directory_exists(args.output_dir)
+        np.save(os.path.join(args.output_dir, args.shape + '_scores.npy'), robustness_scores)
+        np.save(os.path.join(args.output_dir, args.shape + '_success_p.npy'), perturbation_success)
+        np.save(os.path.join(args.output_dir, args.shape + '_outcomes.npy'), outcomes)
+
+    if args.show is True:
+        robustness_scores = np.load(os.path.join(args.output_dir, args.shape + '_scores.npy'))
+        perturbation_success = np.load(os.path.join(args.output_dir, args.shape + '_success_p.npy'))
+        analyse_perturbation_success(perturbation_success)
+        visualise_perturbation_success(perturbation_success)
+
+        print(f'loaded robustness scores for {len(robustness_scores)} grasps, having {len(grasps)} grasps.')
+        print('\tavg:', np.mean(robustness_scores))
+        print('\tmin:', np.min(robustness_scores))
+        print('\tmax:', np.max(robustness_scores))
+        print('\tmed:', np.median(robustness_scores))
+        print(f'\t>0.5: {np.mean(robustness_scores > 0.5)} ({np.count_nonzero(robustness_scores > 0.5)})')
+        print(f'\t>0.75: {np.mean(robustness_scores > 0.75)} ({np.count_nonzero(robustness_scores > 0.75)})')
+        print(f'\t>0.9: {np.mean(robustness_scores > 0.9)} ({np.count_nonzero(robustness_scores > 0.9)})')
+
+        plt.hist(robustness_scores, bins='auto')
+        plt.title('histogram of robustness scores')
+        plt.show()
+
+        robustness_scores = robustness_scores[:len(grasps)]
+        grasps = grasps[:len(robustness_scores)]
+        grasps.scores = robustness_scores
+
+        model_fn = os.path.join(args.model_dir, args.shape + '.obj')
+        mesh = burg.io.load_mesh(model_fn)
+        burg.visualization.show_grasp_set([mesh], grasps, gripper=burg.gripper.ParallelJawGripper(), with_plane=True,
+                                          score_color_func=lambda s: [1-s, s, 0])
 
     print('bye')
