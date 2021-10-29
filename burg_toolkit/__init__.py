66b2aaeab35cebbc51f5f8bb02381a5bf8e7b8bb
Martin Rudorfer <m.rudorfer@bham.ac.uk>
2021-10-29 18:29:36 +0100

    fixed bug that modules could not be found, so had to import them explicitly. no idea why. anyways.

--- burg_toolkit/__init__.py
+++ burg_toolkit/__init__.py
@@ -7,3 +7,12 @@ Benchmarking and Understanding Robotic Grasping
 # directly import frequently used data structures (shortens names during usage)
 from .core import *
 from .grasp import *
+
+# import all other modules
+from . import gripper, io, mesh_processing, metrics, render, sampling, sim, util, visualization
+
+import logging
+# todo: setting debug level for now, will have to change this so applications can override it
+# see https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+logging.basicConfig(level=logging.DEBUG)
+
