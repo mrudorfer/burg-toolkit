e7c0d1553bacb8b0e9ad0b88cf408bbd2db382b1
Martin Rudorfer <m.rudorfer@bham.ac.uk>
2021-11-04 11:10:28 +0000

    added method to scene simulator to simulate complete scenes. there seems to be a bug that pybullet does not use the fixed base for bg_objects

--- burg_toolkit/scene_sim.py
+++ burg_toolkit/scene_sim.py
@@ -47,3 +47,43 @@ class SceneSimulator(SimulatorBase):
 
         object_instance.pose = self._get_body_pose(instance, convert2burg=True)
         return self._simulated_seconds
+
+    def simulate_scene(self, scene):
+        """
+        Simulates the given `scene` for at most `timeout` seconds or until all its containing object instances
+        attain a resting pose. An xy-plane at z=0 will be added and all background objects will be fixed, e.g.
+        cannot move in space.
+        The poses of the instances will be updated after the simulation ends.
+
+        :param scene: core.Scene which shall be simulated
+
+        :return: number of simulated seconds as indicator whether the simulator timed out or not.
+        """
+        self._reset(plane_and_gravity=True)
+        instance_body_ids = {}
+        bg_body_ids = {}
+        for instance in scene.objects:
+            instance_body_ids[instance] = self._add_object(instance)
+        for bg_instance in scene.bg_objects:
+            bg_body_ids[bg_instance] = self._add_object(bg_instance, fixed_base=True)
+
+        steps_below_eps = 0
+        max_steps = self.min_secs_below_eps / self.dt
+        while self._simulated_seconds < self.timeout and steps_below_eps < max_steps:
+            self._step()
+            # check velocities of all objects
+            max_vel = 0
+            for body_id in instance_body_ids.values():
+                vel, angular_vel = self._p.getBaseVelocity(body_id)
+                max_body_vel = np.abs(np.asarray([*vel, *angular_vel])).max()
+                max_vel = max(max_vel, max_body_vel)
+                if max_vel > self.eps:
+                    # don't need to check the other objects if already exceeding threshold
+                    break
+            if max_vel < self.eps:
+                steps_below_eps += 1
+
+        for instance in scene.objects:
+            instance.pose = self._get_body_pose(instance_body_ids[instance], convert2burg=True)
+
+        return self._simulated_seconds
